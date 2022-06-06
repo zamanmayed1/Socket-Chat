f46a8c2be9a786001ba4384acf92af9472f73f70
zamanmayed1 <zamanmayed02@gmail.com>
2022-06-06 19:56:32 +0600

    Socket Configer SuccessFull

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const app = express()
 const http = require('http')
 const expressServer = http.createServer(app)
 
+const { Server } = require("socket.io")
+const io = new Server(expressServer)
+
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+"/index.html")
+    res.sendFile(__dirname + "/index.html")
 })
 
 
+io.on('connection', (socket) => {
+    console.log('User Connected');
 
+    socket.on('disconnect', () => {
+        console.log("User DisConneced");
+    })
+})
 
 
 
